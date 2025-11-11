5f6ef15bf36c747ea74e34d34b7313c8058f8234
MD Harunar Rashid <harunarrashid4093@gmail.com>
2025-11-12 02:37:09 +0600

    commit my ero

--- index.js
+++ index.js
@@ -1,87 +1,159 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const app = express()
+const port = process.env.PORT || 5000
 
 //middleware
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://smartdbUser:s2iYaLEDKGs9cO8B@cluster0.5asayq5.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.5asayq5.mongodb.net/?appName=Cluster0`;
+	
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-
-app.get('/', (req, res) =>{
-    res.send("Smart server is running")
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 })
 
-async function  run (){
-try{
-    await client.connect();
-    
-    const db = client.db('smart_db')
-    const productsCollection = db.collection('products');
+async function run() {
+	try {
+		// await client.connect(); //it has automatic uses here....
 
-    app.get('/products', async (req, res) => {
-        const cursor = productsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-    
-    app.get('/products/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const result = await productsCollection.findOne(query);
-        res.send(result);
-    })
+		const db = client.db('smart_db')
+		const productsCollection = db.collection('products')
+		const bidsCollection = db.collection('bids')
+		const usersCollection = db.collection('users')
+		//USERS API
+		app.post('/users', async (req, res) => {
+			const newUser = req.body
 
+			const email = req.body.email
+			const query = { email: email }
+			const existingUser = await usersCollection.findOne(query)
+			if (existingUser) {
+				res.send({ message: 'user already exits Don`t need to intend' })
+			} else {
+				const result = await usersCollection.insertOne(newUser)
+				res.send(result)
+			}
+			const result = await usersCollection.insertOne(newUser)
+			res.send(result)
+		})
 
-    app.post('/products', async (req, res) =>{
-        const newProduct = req.body;
-        const result = await productsCollection.insertOne(newProduct)
-        res.send(result);
-    })
-    app.patch('/products/:id', async (req, res) =>{
-        const id = req.params.id;
-        const updatedProduct = req.body;
-        const query = { _id: new ObjectId(id)}
-        const update = {
-            $set: {
-                name: updatedProduct.name,
-                price: updatedProduct.price
+		//products APIs
+		app.get('/products', async (req, res) => {
+			// const projectFields = {title: 1, price_min: 1, price_max: 1, image:1 }
+			// const cursor = productsCollection.find().sort({price_min: 1})
+			// .skip(2).limit(2).project(projectFields);
+
+			console.log(req.query)
+			const email = req.query.email
+			const query = {}
+			if (email) {
+				query.email = email
+			}
+			const cursor = productsCollection.find(query)
+			const result = await cursor.toArray()
+			res.send(result)
+		})
+		app.get('/latest-products', async (req, res) => {
+			const cursor = productsCollection.find().sort({ created_at: -1 })
+			const result = await cursor.toArray()
+			res.send(result)
+		})
+
+		app.get('/products/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: id }
+			console.log('query', query)
+			const result = await productsCollection.findOne(query)
+			res.send(result)
+		})
+
+		app.post('/products', async (req, res) => {
+			const newProduct = req.body
+			const result = await productsCollection.insertOne(newProduct)
+			res.send(result)
+		})
+		app.patch('/products/:id', async (req, res) => {
+			const id = req.params.id
+			const updatedProduct = req.body
+			const query = { _id: new ObjectId(id) }
+			const update = {
+				$set: updatedProduct,
+			}
+			const result = await productsCollection.updateOne(query, update)
+			res.send(result)
+		})
+		app.delete('/products/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: new ObjectId(id) }
+			const result = await productsCollection.deleteOne(query)
+			res.send(result)
+		})
+		// bids related api
+		app.get('/bids', async (req, res) => {
+			const email = req.query.email
+			const query = {}
+			if (email) {
+				query.buyer_email = email
+			}
+
+			const cursor = bidsCollection.find(query)
+			const result = await cursor.toArray()
+			res.send(result)
+		})
+		app.get('/products/bids/:productId', async (req, res) => {
+			const productId = req.params.productId
+			const query = { product: productId }
+			const cursor = bidsCollection.find(query).sort({ bid_price: -1 })
+			const result = await cursor.toArray()
+			res.send(result)
+		})
+        app.get('/bids', async(req, res) =>{
+
+            const query = {};
+            if(query.email){
+                query.buyer_email = email;
+
             }
-        }
-        const result = await  productsCollection.updateOne(query, update )
-        res.send(result)
-    })
-    app.delete('/products/:id', async (req, res) => {
-        const id = req.params.id;
-        const query  = { _id: new ObjectId(id)}
-        const result = await productsCollection.deleteOne(query);
-        res.send(result);
+            const cursor = bidsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+		app.post('/add-bid', async (req, res) => {
+			const newBid = req.body
+			console.log(newBid)
+			const result = await bidsCollection.insertOne(newBid)
+			res.send(result)
+		})
 
-    }) 
-    await client.db("admin").command({ping:1});
-    console.log("Pinged Your deployment. You successfully connected to MongDb!");
+		app.delete('/bids/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: new ObjectId(id) }
+			const result = await bidsCollection.deleteOne(query)
+			res.send(result)
+		})
 
-}
-finally{
-
-}
+		console.log('Pinged Your deployment. You successfully connected to MongDb!')
+	} finally {
+		// this is finally block
+	}
 }
 
 run().catch(console.dir)
 
-app.listen(port, () =>{
-    console.log(`Smart server is running on port: ${port}`);
+app.get('/', (req, res) => {
+	res.send('Smart server is running')
+})
+
+app.listen(port, () => {
+	console.log(`Smart server is running on port: ${port}`)
 })
